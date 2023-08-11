120f13791d218985d0a0f404fa138770afd5f177
Giancarlo Sanchez <gianksp@gmail.com>
2023-08-11 14:30:44 +0100

    Smart contract SBT

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import { ThirdwebProvider } from '@thirdweb-dev/react';
 import '../styles/globals.css';
+import ChainContext from '../context/chain';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = 'ethereum';
+
 
 function MyApp({ Component, pageProps }) {
+	const [selectedChain, setSelectedChain] = useState("optimism");
 	return (
-		<ThirdwebProvider
-			activeChain={activeChain}
-			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-		>
-			<Component {...pageProps} />
-		</ThirdwebProvider>
+		<ChainContext.Provider value={{ selectedChain, setSelectedChain }}>
+			<ThirdwebProvider
+				activeChain={selectedChain}
+				clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+			>
+				<Component {...pageProps} />
+			</ThirdwebProvider>
+		</ChainContext.Provider>
 	);
 }
 
